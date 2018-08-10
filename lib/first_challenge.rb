cb02f112b853fb9dea6f77873968d447f3060d6f
Patricia Carter <patricia4194@gmail.com>
2018-08-10 20:51:57 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,11 @@ def first_challenge
     }
   }
 
-  #your code here
-
+  contacts["Freddy Mercury"].each do |name, data|
+    if name == :favorite_icecream_flavors
+      data.delete_if {|x| x = "strawberry"}
+      end 
+    end
 
   #remember to return your newly altered contacts hash!
   contacts
